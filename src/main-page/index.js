6389847c9f2cd892921cf875c8b809ead6ad0f6c
sandro_herbst <sandro.herbst@gmail.com>
2019-11-23 19:18:49 -0300

    FeaturedHouse

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './main-page.css';
+import React, {Component} from 'react'
+import './main-page.css'
 import Header from './header'
 
-function App() {
-  return (
-    <div className='container'>
-      <Header subtitle='Providing houses all over world'/>
-    </div>
-  );
+class App extends Component {
+
+//  constructor(props) {
+//    super(props)
+//    this.state = {};
+//  }
+
+  state = {}
+
+  componentDidMount() {
+    this.fetchHouses()
+  }
+
+  fetchHouses = () => {
+    fetch('/houses.json')
+    .then(res => res.json())
+    .then(allHouses => {
+      this.allHouses = allHouses
+      this.determineFeaturedHouse()
+    })
+  }
+
+  determineFeaturedHouse = () => {
+    if (this.allHouses) {
+      const randomIndex = Math.floor(Math.random() * this.allHouses.length)
+      const featuredHouse =  this.allHouses[randomIndex]
+      this.setState({ featuredHouse })
+    }
+  }  
+
+  render() {
+    return (
+      <div className='container'>
+        <Header subtitle='Providing houses all over world'/>
+      </div>
+    );
+  }
 }
 
 export default App;
